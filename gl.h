72fa5925026e62525c1606ad6e9087043f539920
Luca Mazza <luca.mazza@mazluc.ch>
2026-03-29 21:56:00 +0200

    gl.h

---
+++ gl.h
@@ -0,0 +1,51 @@
+/* gl - v0.1 - public domain graphics library - https://mazluc.ch/tiny
+                             no warranty implied; use at your own risk
+
+   Do this:
+      #define GL_IMPLEMENTATION
+   before you include this file in *one* C or C++ file to create the implementation
+
+   // It should look like this:
+   #include ...
+   #define GL_IMPLEMENTATION
+   #include "gl.h"
+
+   QUICK NOTES:
+      ...
+      Full documentation under DOCUMENTATION below.
+
+   LICENSE:
+
+      See end of file for license information.
+
+   RECENT REVISION HISTORY:
+      ...
+      See end of file for full revision history.
+
+   ============================    Contributors    =========================
+
+*/
+#ifndef GL_H
+#define GL_H
+
+// DOCUMENTATION
+//
+// Limitations:
+//
+// Basic usage:
+//
+// Standard parameters:
+//
+// ===========================================================================
+//
+// Philosophy:
+//
+// ===========================================================================
+//
+// Additional configurations:
+#endif
+
+#ifdef GL_IMPLEMENTATION
+
+
+#endif
